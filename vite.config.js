daafca48c6b1f4fc3f12115a5dd36e23837b4c7c
Liang Jin Tan <liangjin0228@gmail.com>
2023-11-25 00:36:53 +0800

    change front end dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,34 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+import ViteFonts from "unplugin-fonts/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
+        }),
+        vue({
+            template: { transformAssetUrls },
+        }),
+        // https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin#readme
+        vuetify({
+            autoImport: true,
+            styles: {
+                configFile: "resources/css/settings.scss",
+            },
+        }),
+        ViteFonts({
+            google: {
+                families: [
+                    {
+                        name: "Roboto",
+                        styles: "wght@100;300;400;500;700;900",
+                    },
+                ],
+            },
         }),
     ],
 });

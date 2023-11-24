a12ec4df1f1d5009a601ab2804739ebb1d13e460
Liang Jin Tan <liangjin0228@gmail.com>
2023-11-25 00:37:56 +0800

    change sample view

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
-import './bootstrap';
+import "./bootstrap";
+
+// Plugins
+import { registerPlugins } from "@/plugins";
+
+// Components
+import App from "./components/App.vue";
+
+// Composables
+import { createApp } from "vue";
+
+const app = createApp(App);
+
+registerPlugins(app);
+
+app.mount("#app");
---
+++ resources/js/components/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <v-container>
+        <v-card>
+            <v-card-title>This is a sample app</v-card-title>
+        </v-card>
+    </v-container>
+</template>

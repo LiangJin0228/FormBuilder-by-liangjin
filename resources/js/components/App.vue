d65d022562f89f68bb66fa416b5f5a4afe099af2
LiangJinTan <liangjin0228@gmail.com>
2023-11-27 00:00:34 +0800

    structure

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,7 +1,36 @@
 <template>
-    <v-container>
-        <v-card>
-            <v-card-title>This is a sample app</v-card-title>
-        </v-card>
-    </v-container>
+    <v-app class="rounded rounded-md">
+        <v-app-bar title="Application bar"></v-app-bar>
+
+        <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+            Main Content
+        </v-main>
+
+        <v-footer class="bg-grey-lighten-1">
+            <v-row justify="center" no-gutters>
+                <v-btn v-for="link in links" :key="link" color="white" variant="text" class="mx-2" rounded="xl">
+                    {{ link }}
+                </v-btn>
+                <v-col class="text-center mt-4" cols="12">
+                    {{ new Date().getFullYear() }} - <strong>Form Builder</strong>
+                </v-col>
+            </v-row>
+        </v-footer>
+        <v-bottom-navigation>Powered by Liang Jin</v-bottom-navigation>
+    </v-app>
 </template>
+
+<script>
+export default {
+    data: () => ({
+        links: [
+            'Home',
+            'About Us',
+            'Team',
+            'Services',
+            'Blog',
+            'Contact Us',
+        ],
+    }),
+}
+</script>
